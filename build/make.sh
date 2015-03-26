f0fa597a87e809ec4835d0939244229e2034c835
Seth Woodworth <seth@sethish.com>
2015-03-26 01:02:09 -0400

    adding reqs and installing to make

--- build/make.sh
+++ build/make.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-epubmaker --title="`cat pg48562.rdf | grep title | sed 's|<[^>]*>||g'`" --author="`cat pg48562.rdf | grep name | sed 's|<[^>]*>||g' | tr -d '\n'`" --input-encoding='text/html' 48562-h/48562-h.htm 
+pip install -r build/requirements.txt;
+epubmaker --title="`cat pg48562.rdf | grep title | sed 's|<[^>]*>||g'`" --author="`cat pg48562.rdf | grep name | sed 's|<[^>]*>||g' | tr -d '\n'`" --input-encoding='text/html' 48562-h/48562-h.htm;
