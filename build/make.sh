c7ae7c1d3c9c308116599d9466221c5c0ab2a478
Seth Woodworth <seth@sethish.com>
2015-03-26 01:57:22 -0400

    fix html path

--- build/make.sh
+++ build/make.sh
@@ -4,4 +4,4 @@
 echo "`pwd`";
 pip install PIL --allow-external PIL --allow-unverified PIL;
 pip install -r build/requirements.txt;
-epubmaker --title="`cat pg48562.rdf | grep title | sed 's|<[^>]*>||g'`" --author="`cat pg48562.rdf | grep name | sed 's|<[^>]*>||g' | tr -d '\n'`" --input-encoding='text/html' `pwd`../48562-h/48562-h.htm;
+epubmaker --title="`cat pg48562.rdf | grep title | sed 's|<[^>]*>||g'`" --author="`cat pg48562.rdf | grep name | sed 's|<[^>]*>||g' | tr -d '\n'`" --input-encoding='text/html' `pwd`/48562-h/48562-h.htm;
