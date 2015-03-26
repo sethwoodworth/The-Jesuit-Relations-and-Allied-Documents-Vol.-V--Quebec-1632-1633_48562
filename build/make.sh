d9383868e95841af8786c377ede008698255923d
Seth Woodworth <seth@sethish.com>
2015-03-26 01:08:12 -0400

    adding PIL specfic install

--- build/make.sh
+++ build/make.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+pip install PIL --allow-external PIL --allow-unverified PIL;
 pip install -r build/requirements.txt;
 epubmaker --title="`cat pg48562.rdf | grep title | sed 's|<[^>]*>||g'`" --author="`cat pg48562.rdf | grep name | sed 's|<[^>]*>||g' | tr -d '\n'`" --input-encoding='text/html' 48562-h/48562-h.htm;
