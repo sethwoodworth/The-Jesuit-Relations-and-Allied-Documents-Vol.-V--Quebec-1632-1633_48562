f1aa091378999323583a0ff11b63345a61d94b45
Seth Woodworth <seth@sethish.com>
2015-03-26 01:38:47 -0400

    trying relative paths

--- build/make.sh
+++ build/make.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 pip install PIL --allow-external PIL --allow-unverified PIL;
 pip install -r build/requirements.txt;
-epubmaker --title="`cat pg48562.rdf | grep title | sed 's|<[^>]*>||g'`" --author="`cat pg48562.rdf | grep name | sed 's|<[^>]*>||g' | tr -d '\n'`" --input-encoding='text/html' 48562-h/48562-h.htm;
+epubmaker --title="`cat pg48562.rdf | grep title | sed 's|<[^>]*>||g'`" --author="`cat pg48562.rdf | grep name | sed 's|<[^>]*>||g' | tr -d '\n'`" --input-encoding='text/html' `pwd`../48562-h/48562-h.htm;
