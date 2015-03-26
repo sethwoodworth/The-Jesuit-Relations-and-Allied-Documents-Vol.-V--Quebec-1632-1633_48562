b906a4a856517a4026e5ea1f70e61832de7daa09
Seth Woodworth <seth@sethish.com>
2015-03-26 01:39:14 -0400

    add echo path

--- build/make.sh
+++ build/make.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
+echo '`pwd`';
 pip install PIL --allow-external PIL --allow-unverified PIL;
 pip install -r build/requirements.txt;
 epubmaker --title="`cat pg48562.rdf | grep title | sed 's|<[^>]*>||g'`" --author="`cat pg48562.rdf | grep name | sed 's|<[^>]*>||g' | tr -d '\n'`" --input-encoding='text/html' `pwd`../48562-h/48562-h.htm;
